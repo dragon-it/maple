1bdc1b3242bd88366d21c1176d32e0cb7f147093
LeeYongSoo <sideoff0217@naver.com>
2024-01-28 02:55:12 +0900

    feat:리셋 css 적용, header, footer 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import { Footer } from './components/common/Footer';
+import { Header } from './components/common/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+        <Header/>
+        <Footer/>
     </div>
   );
 }
 
 export default App;
+
