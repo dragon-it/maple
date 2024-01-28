69af6a3506cf408aadd8db186c733916730ed581
LeeYongSoo <sideoff0217@naver.com>
2024-01-29 00:30:08 +0900

    fix/node 14버전 사용

--- src/components/main/Search.jsx
+++ src/components/main/Search.jsx
@@ -33,7 +33,7 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 200px;
+  margin-top: 150px;
   width: 100%;
   height: 50px;
   z-index: 99;
