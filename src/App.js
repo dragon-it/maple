67f7edb7e526920557e1be92dac467c85dc67496
LeeYongSoo <sideoff0217@naver.com>
2024-01-30 14:39:26 +0900

    feat:기본정보 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import { Main } from './pages/Main';
@@ -8,17 +7,16 @@ import { User } from './pages/User';
 import { Footer } from './components/common/Footer';
 import { BackgroundImage } from './components/main/BackgroundImage';
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user" element={<User />} />
+        <Route path="/user/:characterName" element={<User />} />
       </Routes>
-      <Footer/>
-      <BackgroundImage/>
+      <Footer />
+      <BackgroundImage />
     </Router>
   );
 }
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BasicInfomation } from '../components/user/BasicInfomation'
 
 
 
 export const User = () => {
   return (
-    <div>User</div>
+    <Container>
+      <BasicInfomation></BasicInfomation>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  
+`
