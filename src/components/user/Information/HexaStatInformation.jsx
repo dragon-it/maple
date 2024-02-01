fb6f7f3e03fd3c39a1d3b8a148cd38300f4c4055
dragon <sideoff0217@naver.com>
2024-02-01 17:08:18 +0900

    feat:헥사 스탯 정보 추가 완료, 부스탯 추가 필요

--- src/components/user/Information/HexaStatInformation.jsx
+++ src/components/user/Information/HexaStatInformation.jsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import hexaStatData from './HexaStatData';
 
-const hexaStatData = [
-  { level: 1, main_stat_name: '공격력 증가', main_stat_level: 5 },
-  { level: 2, main_stat_name: '공격력 증가', main_stat_level: 10 },
-  { level: 3, main_stat_name: '공격력 증가', main_stat_level: 15 },
-  { level: 4, main_stat_name: '공격력 증가', main_stat_level: 20 },
-  { level: 5, main_stat_name: '공격력 증가', main_stat_level: 30 },
-  { level: 6, main_stat_name: '공격력 증가', main_stat_level: 40 },
-  { level: 7, main_stat_name: '공격력 증가', main_stat_level: 50 },
-  { level: 8, main_stat_name: '공격력 증가', main_stat_level: 65 },
-  { level: 9, main_stat_name: '공격력 증가', main_stat_level: 80 },
-  { level: 10, main_stat_name: '공격력 증가', main_stat_level: 100 },
-];
 
-// 다른 데이터 추가 필요
-const renderHexaStat = (hexaStat) => (
-  <div key={hexaStat.level}>
-    <div>Lv.{hexaStat.level} {hexaStat.main_stat_name}: {hexaStat.main_stat_level}</div>
-  </div>
-);
 
 export const HexaStatInformation = ({ HexaStatInfo }) => {
   const hexaStatInfo = HexaStatInfo.character_hexa_stat_core[0];
 
-  const statLevelData = hexaStatData.find(stat => stat.level === hexaStatInfo.main_stat_level);
+  // hexaStatData에서 동일한 이름과 레벨을 가진 정보를 찾아옴
+  const statLevelData = hexaStatData.find((statData) =>
+  statData.main_stat_name === hexaStatInfo.main_stat_name &&
+  statData.level === hexaStatInfo.main_stat_level
+);
+
+    console.log(statLevelData)
 
   return (
     <Container>
-      <div>메인 헥사스텟: {hexaStatInfo.main_stat_name}{renderHexaStat(statLevelData)}</div>
-
-
+      <div>Lv.{statLevelData.level}{hexaStatInfo.main_stat_name} : {statLevelData.main_stat_level}</div>
+      // 부스탯 추가 필요
     </Container>
   );
 };
