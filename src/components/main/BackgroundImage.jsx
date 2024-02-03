07146ba070e6cd5f13e2a1ea5d5ea45c45a1c4a9
LeeYongSoo <sideoff0217@naver.com>
2024-02-03 11:17:53 +0900

    feat: 다크 모드, 라이트 모드

--- src/components/main/BackgroundImage.jsx
+++ src/components/main/BackgroundImage.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import whiteBGI from '../../assets/Henesys3.png';
+import darkBGI from '../../assets/Kerning City.jpg';
+
 
 export const BackgroundImage = () => {
-  const [imageSrc, setImageSrc] = useState('./assets/Henesys3.png');
+  const [imageSrc, setImageSrc] = useState(whiteBGI);
 
   const handleClick = () => {
     // 클릭할 때마다 이미지 변경
     setImageSrc((prevImageSrc) => 
-      prevImageSrc === './assets/Henesys3.png'
-        ? './assets/Kerning City.png'
-        : './assets/Henesys3.png'
+      prevImageSrc === whiteBGI
+        ? darkBGI
+        : whiteBGI
     );
   };
 
@@ -25,7 +28,6 @@ const Container = styled.div`
   z-index: 9;
   img {
     object-fit: cover;
-    opacity: 0.9;
     width: 100%;
     height: 95vh;
     left: 0;
