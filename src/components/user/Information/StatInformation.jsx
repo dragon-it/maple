e7252cf8ecbca8cf6791307dbe30937555413fdd
LeeYongSoo <sideoff0217@naver.com>
2024-02-08 19:06:05 +0900

    feat:캐릭터 스탯 ui및 수치 적용

--- src/components/user/Information/StatInformation.jsx
+++ src/components/user/Information/StatInformation.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+
+import detailBackgrndImg from '../../../assets/ui/basicUi/StatDetailBackgrnd.png';
 import backgrndImg from '../../../assets/ui/basicUi/combatPowerUi.png';
 import attackFont from '../../../assets/ui/basicUi/attackFont.png';
+import mainFont from '../../../assets/ui/basicUi/mainStatFont.png';
+import mainStatUi from '../../../assets/ui/basicUi/mainStat.png';
+import utilityUi from '../../../assets/ui/basicUi/utilityUi.png';
+import utilityFont from '../../../assets/ui/basicUi/utilityFont.png';
 
 export const StatInformation = ({ statInfo }) => {
   const formatNumber = (value) => {
@@ -33,21 +39,27 @@ export const StatInformation = ({ statInfo }) => {
 
   return (
     <Container>
-      <img src={backgrndImg} alt="backgrndImg" />
-      <Combatpower>{formatNumber(statInfo.final_stat[42].stat_value)}</Combatpower>
+      <Combatpower>
+        <img src={backgrndImg} alt="backgrndImg" />
+        <p>{formatNumber(statInfo.final_stat[42].stat_value)}</p>
+      </Combatpower>
+
       <StatWrap>
+        <img src={mainStatUi} alt="mainStatUi" />
+        <TextWrap>
           <TextLeft>
-          <p>{statInfo.final_stat[20].stat_value}</p>
-          <p>{statInfo.final_stat[16].stat_value}</p>
-          <p>{statInfo.final_stat[18].stat_value}</p>
+            <p>{statInfo.final_stat[20].stat_value}</p>
+            <p>{statInfo.final_stat[16].stat_value}</p>
+            <p>{statInfo.final_stat[18].stat_value}</p>
           </TextLeft>
           <TextRight>
-          <p>{statInfo.final_stat[21].stat_value}</p>
-          <p>{statInfo.final_stat[17].stat_value}</p>
-          <p>{statInfo.final_stat[19].stat_value}</p>
+            <p>{statInfo.final_stat[21].stat_value}</p>
+            <p>{statInfo.final_stat[17].stat_value}</p>
+            <p>{statInfo.final_stat[19].stat_value}</p>
           </TextRight>
+        </TextWrap>
         <StatUi>
-
+          <img src={mainFont} alt="mainFont" />
         </StatUi>
       </StatWrap>
       <AttackFontWrap>
@@ -76,54 +88,92 @@ export const StatInformation = ({ statInfo }) => {
         </FontUi>
       </AttackFontWrap>
       <EtcStatWrap>
-        <TextLeft>
-            <p>{statInfo.final_stat[29].stat_value}</p>
-            <p>{statInfo.final_stat[28].stat_value}</p>
-            <p>{statInfo.final_stat[39].stat_value}</p>
-            </TextLeft>
-            <TextRight>
+        <img src={utilityUi} alt="utilityUi" />
+        <EtcTextWrap>
+          <TextLeft>
+            <p>{statInfo.final_stat[29].stat_value}%</p>
+            <p>{statInfo.final_stat[28].stat_value}%</p>
+            <p>{statInfo.final_stat[39].stat_value}%</p>
+          </TextLeft>
+          <TextRight>
             <p>{statInfo.final_stat[13].stat_value}</p>
             <p>{statInfo.final_stat[14].stat_value}</p>
             <p>{statInfo.final_stat[15].stat_value}</p>
-            </TextRight>
-          <EtcUi></EtcUi>
+          </TextRight>
+        </EtcTextWrap>
+        <EtcUi>
+          <img src={utilityFont} alt="utilityFont" />
+        </EtcUi>
       </EtcStatWrap>
     </Container>
-  )
-}
+  );
+};
+
+const commonTextStyle = `
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  font-size: 14px;
+  color: white;
+  gap: 8px;
+`;
 
 const Container = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 472px;
+  height: 479px;
   position: relative;
-  background-image: url(${backgrndImg});
+  display: flex;
+  justify-content: center;
   background-repeat: no-repeat;
-  text-shadow: 1px 1px rgba(0,0,0, 0.25);
-`
+  background-image: url(${detailBackgrndImg});
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+`;
 
 const Combatpower = styled.div`
-  color: rgb(255,250,210);
+  color: rgb(255, 250, 210);
   display: flex;
-  justify-content:center;
+  justify-content: center;
   position: absolute;
-  top: 7px;
+  top: 37px;
   width: 100%;
   font-size: 20px;
-`
+  p {
+    position: absolute;
+    width: 100%;
+    height: 33px;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const StatWrap = styled.div`
-position: absolute;
-display: flex;
-align-items: flex-end;
-justify-content: flex-end;
-width: 100%;
-top: 50px;
-gap: 170px;
-background-color: red;
-`
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-end;
+  width: 448px;
+  height: 81px;
+  top: 75px;
+  gap: 170px;
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+  }
+`;
 
 const StatUi = styled.div`
-`
-
+  img {
+    width: 242px;
+    height: 53px;
+    position: absolute;
+    left: 13px;
+    top: 13px;
+  }
+`;
 
 const AttackFontWrap = styled.div`
   display: flex;
@@ -132,47 +182,72 @@ const AttackFontWrap = styled.div`
   justify-content: flex-end;
   gap: 170px;
   position: absolute;
-  bottom: 0px;
-  padding-top: 10px;
+  bottom: 131px;
+  left: 12px;
   width: 448px;
   height: 190px;
+  padding: 10px 0;
+`;
+
+const TextWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  gap: 190px;
   padding-bottom: 13px;
-`
+  padding-top: 12px;
+`;
 
 const TextRight = styled.div`
+  ${commonTextStyle}
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  font-size: 14px;
   padding-right: 10px;
-  color: white;
-  gap: 8px;
-`
+`;
 
 const TextLeft = styled.div`
+  ${commonTextStyle}
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  font-size: 14px;
   padding-left: 10px;
-  color: white;
-  gap: 8px;
-`
+`;
 
 const FontUi = styled.div`
   position: absolute;
   left: 13px;
-`
+`;
 
 const EtcStatWrap = styled.div`
   position: absolute;
-display: flex;
-right: 0;
-bottom: 0;
-gap: 30px;
-background-color: red;
-`
+  display: flex;
+  width: 448px;
+  height: 92px;
+  bottom: 35px;
+  gap: 30px;
+`;
+
+const EtcTextWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: flex-end;
+  gap: 198px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+`;
 
 const EtcUi = styled.div`
-  
-`
+  img {
+    position: absolute;
+    left: 14px;
+    top: 13px;
+  }
+`;
+
+export default StatInformation;
