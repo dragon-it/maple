74cbbf5d91de55f12e185aa4a739cbe71cf15898
LeeYongSoo <sideoff0217@naver.com>
2024-02-14 02:44:29 +0900

    feat: 코드 단순화, 등급에 따른 테두리 지정

--- src/components/user/equipment/ItemEquipmentInformation.jsx
+++ src/components/user/equipment/ItemEquipmentInformation.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import equipmentUi from '../../../assets/ui/equipmentUi/equipUi.png'
 
 export const ItemEquipmentInformation = ({ EquipData }) => {
+  const [selectedPreset, setSelectedPreset] = useState('preset1');
+  const positions = {    
+    '모자': {top: '62px', left: '158px'},
+    '얼굴장식': {top: '112px', left: '158px'},
+    '눈장식': {top: '161px', left: '158px'},
+    '귀고리': {top: '161px', left: '207px'},
+    '상의': {top: '210px', left: '158px'},
+    '하의': {top: '260px', left: '158px'},
+    '신발': {top: '309px', left: '158px'},
+    '장갑': {top: '260px', left: '207px'},
+    '망토': {top: '260px', left: '256px'},
+    '보조무기': {top: '210px', left: '256px'},
+    '무기': {top: '210px', left: '109px'},
+    '반지1': {top: '63px', left: '60px'},
+    '반지2': {top: '112px', left: '60px'},
+    '반지3': {top: '161px', left: '60px'},
+    '반지4': {top: '210px', left: '60px'},
+    '펜던트': {top: '112px', left: '109px'},
+    '훈장': {top: '161px', left: '257px'},
+    '벨트': {top: '260px', left: '109px'},
+    '어깨장식': {top: '210px', left: '207px'},
+    '포켓 아이템': {top: '260px', left: '60px'},
+    '기계 심장': {top: '308px', left: '257px'},
+    '뱃지': {top: '112px', left: '257px'},
+    '엠블렘': {top: '62px', left: '257px'},
+    '펜던트2': {top: '161px', left: '109px'}
+    };
 
+    const gradeColors = {
+      '레어': 'rgb(85, 170, 255)',
+      '에픽': 'rgb(204, 102, 255)',
+      '유니크': 'rgb(255, 204, 0)',
+      '레전드리': 'rgb(0, 255, 0)',
+    };
+
+    const ItemIcon = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
+  width: 46px;
+  height: 46px;
+  cursor: pointer;
+  border: ${({ grade, gradeColors }) => `2px solid ${gradeColors[grade] || 'none'}`};
+`;
+
+  const handlePresetChange = (preset) => {
+    setSelectedPreset(preset);
+  };
 
   return (
     <Container>
@@ -11,19 +60,28 @@ export const ItemEquipmentInformation = ({ EquipData }) => {
         <img src={equipmentUi} alt="ui" />
       </UiBackgrnd>
       <EquipItems>
-        {EquipData.item_equipment.map((item, index) => (
-          <ItemIcon key={index}>
-            <img src={item.item_icon} alt={`icon-${index}`} />
-          </ItemIcon>
-        ))}
+      {EquipData[`item_equipment_${selectedPreset}`].map((item, index) => (
+  <ItemIcon 
+    key={index} 
+    style={positions[item.item_equipment_slot]} 
+    grade={item.potential_option_grade}
+    gradeColors={gradeColors}
+  >
+    <img src={item.item_icon} alt={`icon-${index}`} />
+  </ItemIcon>
+))}
       </EquipItems>
+      <PresetButtons>
+        <button onClick={() => handlePresetChange('preset1')}>Preset 1</button>
+        <button onClick={() => handlePresetChange('preset2')}>Preset 2</button>
+        <button onClick={() => handlePresetChange('preset3')}>Preset 3</button>
+      </PresetButtons>
     </Container>
   );
 };
 
 const Container = styled.div`
   white-space: nowrap;
-
 `;
 
 const UiBackgrnd = styled.div`
@@ -31,146 +89,17 @@ const UiBackgrnd = styled.div`
   img{
     width: 262px;
     height: 312px;
+    opacity: 0.8;
   }
-`
+`;
 
 const EquipItems = styled.div`
-display: flex;
-width: 500px;
-flex-wrap: wrap;
-div{
-  position: absolute;
-}
-// 모자 
-div:nth-child(1){
-    left: 158px;
-    top: 62px;
-}
-// 얼굴장식
-div:nth-child(2){
-    left: 158px;
-    top: 112px;
-} 
-// 눈장식
-div:nth-child(3){
-    left: 158px;
-    top: 161px;
-}
-// 귀고리
-div:nth-child(4){
-    left: 207px;
-    top: 161px;
-}
-// 상의
-div:nth-child(5){
-    left: 158px;
-    top: 210px;
-}
-// 하의
-div:nth-child(6){
-    left: 158px;
-    top: 260px;
-}
-// 신발
-div:nth-child(7){
-    left: 158px;
-    top: 309px;
-}
-// 장갑
-div:nth-child(8){
-    left: 207px;
-    top: 260px;
-}
-// 망토
-div:nth-child(9){
-    left: 256px;
-    top: 260px;
-}
-// 보조 무기
-div:nth-child(10){
-    left: 256px;
-    top: 210px;
-}
-// 무기
-div:nth-child(11){
-    left: 109px;
-    top: 210px;
-}
-// 반지
-div:nth-child(12){
-    left: 60px;
-    top: 63px;
-}
-// 반지
-div:nth-child(13){
-    left: 60px;
-    top: 112px;
-}
-// 반지
-div:nth-child(14){
-    left: 60px;
-    top: 161px;
-}
-// 반지
-div:nth-child(15){
-    left: 60px;
-    top: 210px;
-}
-// 펜던트
-div:nth-child(16){
-    left: 109px;
-    top: 112px;
-}
-// 훈장
-div:nth-child(17){
-    left: 257px;
-    top: 161px;
-}
-// 벨트
-div:nth-child(18){
-    left: 109px;
-    top: 260px;
-}
-// 어깨장식
-div:nth-child(19){
-    left: 207px;
-    top: 210px;
-}
-// 포켓 아이템
-div:nth-child(20){
-    left: 60px;
-    top: 260px;
-}
-// 기계 심장
-div:nth-child(21){
-    left: 257px;
-    top: 308px;
-}
-// 뱃지
-div:nth-child(22){
-    left: 257px;
-    top: 112px;
-}
-// 엠블렘
-div:nth-child(23){
-    left: 257px;
-    top: 62px;
-}
-// 펜던트
-div:nth-child(24){
-    left: 109px;
-    top: 161px;
-}
+  display: flex;
+  width: 500px;
+  flex-wrap: wrap;
 `;
 
-const ItemIcon = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 99;
-  width: 46px;
-  height: 46px;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: #e0a7a7;
-`;
+
+const PresetButtons = styled.div`
+  
+`
