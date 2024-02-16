a38730d13a105dd06949a853aa77d8394161da18
LeeYongSoo <sideoff0217@naver.com>
2024-02-16 20:18:34 +0900

    feat: 스타포스 적용 및 기본 옵션 설정 완료

--- src/components/user/Information/BasicInformation.jsx
+++ src/components/user/Information/BasicInformation.jsx
@@ -6,12 +6,12 @@ export const BasicInformation = ({ BasicInfo }) => {
   return (
     <Container>
       <div><img src={BasicInfo.getBasicInformation.character_image} alt={BasicInfo.character_name} /></div>
-      <div>캐릭터 이름: {BasicInfo.getBasicInformation.character_name}</div>
+      <div>{BasicInfo.getBasicInformation.character_name}</div>
       <div>직업: {BasicInfo.getBasicInformation.character_class}</div>
       <div>레벨: {BasicInfo.getBasicInformation.character_level}</div>
       <div>길드: {BasicInfo.getBasicInformation.character_guild_name}</div>
       <div>차수: {BasicInfo.getBasicInformation.character_class_level}차</div>
-      <div>경험치 비율: {BasicInfo.getBasicInformation.character_exp_rate}%</div>
+      <div>경험치: {BasicInfo.getBasicInformation.character_exp_rate}%</div>
       <div>월드: {BasicInfo.getBasicInformation.world_name}</div>
       <div>인기도: {BasicInfo.getCharacterPopularity.popularity}</div>
     </Container>
