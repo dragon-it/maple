96dd56979907587e2eff65ac1e57a677908ef684
LeeYongSoo <sideoff0217@naver.com>
2024-03-08 05:53:33 +0900

    feat: 하이퍼 스탯 완성

--- src/components/user/Information/HyperStatInformation.jsx
+++ src/components/user/Information/HyperStatInformation.jsx
@@ -15,66 +15,101 @@ export const HyperStatInformation = ({ HyperStatInfo }) => {
 
   return (
     <Container>
-      <div>Hyper Stat</div>
-      <StatWrap>
-        {filteredStats.map((stat, index) => (
-          <StatInfo
-            key={index}>
-            <StatContainer>
-              <div>{stat.stat_increase}</div>
-              <div>Lv.{stat.stat_level}</div>
-            </StatContainer>
-          </StatInfo>
-        ))}
-      </StatWrap>
-      <div>POINT: {HyperStatInfo[`${currentPresetKey}_remain_point`]}</div>
-      <ButtonContainer>
-      <div>프리셋</div>
-        {[1, 2, 3].map((presetNumber) => (
-          <PresetButton
-            key={presetNumber}
-            onClick={() => handlePresetChange(presetNumber)}
-            isSelected={selectedPreset === presetNumber}
-          >
-            {presetNumber}
-          </PresetButton>
-        ))}
-      </ButtonContainer>
+      <HyperHeader>HYPER STAT</HyperHeader>
+      <HyperBody>
+        <StatWrap>
+          {filteredStats.map((stat, index) => (
+            <StatInfo
+              key={index}>
+              <StatContainer>
+                <div>{stat.stat_increase}</div>
+                <Level>Lv.{stat.stat_level}</Level>
+              </StatContainer>
+            </StatInfo>
+          ))}
+        </StatWrap>
+        <ButtonContainer>
+          <PresetWrap>
+            <PresetHeader>PRESET</PresetHeader>
+            {[1, 2, 3].map((presetNumber) => (
+              <PresetButton
+                key={presetNumber}
+                onClick={() => handlePresetChange(presetNumber)}
+                isSelected={selectedPreset === presetNumber}
+              >
+                {presetNumber}
+              </PresetButton>
+            ))}
+          </PresetWrap>
+          <RemainPoint><div>POINT :</div> {HyperStatInfo[`${currentPresetKey}_remain_point`]}</RemainPoint>
+        </ButtonContainer>
+      </HyperBody>
     </Container>
   );
 };
 
 
 const Container = styled.div`
-  padding: 0 10px;
+  padding: 5px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   width: 300px;
-  background-color: #ffffff;
-  font-size: 12px;
+  font-size: 13px;
   border-radius: 10px;
-  height: 300px;
+  border: 1px solid rgb(80,92,101);
+  outline: 1px solid rgb(42,49,58);
+  border-radius: 5px;
+  background-color: rgba(59,66,75, 0.9);
 `;
+
+const HyperHeader = styled.div`
+  font-size: 15px;
+  font-weight: 700;
+  color: rgb(220,252,2);
+  margin: 7px 0;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+`
+const HyperBody = styled.div`
+  border-radius: 5px;
+`
 
 const StatWrap = styled.div`
   margin-bottom: 10px;
+  background-color: rgb(134,148,160);
+  border-radius: 5px;
+  color: white;
+  padding: 7px;
 `
 
 const StatInfo = styled.div`
   padding: 5px 0;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
 `;
+
+const Level = styled.div`
+  width: 30px;
+  display: flex;
+  justify-content: flex-start;
+`
 
 const ButtonContainer = styled.div`
     display: flex;
-    gap: 10px;
     align-items: center;
-    width: 150px;
+    gap: 18px;
     height: 25px;
-    padding: 0 5px;
+    padding: 0 3px;
     background-color: #aaa9a9;
     border-radius: 5px;
+    font-family: maple-light;
 `;
+
+const PresetWrap = styled.div`
+    display: flex;
+  align-items: center;
+  gap: 5px;
+  margin-left: 2px;
+`
 
 const PresetButton = styled.button`
   border: none;
@@ -84,9 +119,28 @@ const PresetButton = styled.button`
     props.isSelected? `filter: brightness(0.5);`: ''}
 `;
 
+const PresetHeader = styled.div`
+  font-size: 13px;
+  text-shadow: none;
+`
+
 const StatContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
+
+const RemainPoint = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 50%;
+  height: 20px;
+  padding: 0 3px;
+  font-size: 12px;
+  border-radius: 5px;
+  background-color: rgba(59,66,75, 0.9);
+  color: white;
+`
 export default HyperStatInformation;
