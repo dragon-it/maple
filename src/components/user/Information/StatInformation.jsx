4e190fbcc738bcb666525315545e93419fcd3850
LeeYongSoo <sideoff0217@naver.com>
2024-03-09 05:31:09 +0900

    feat: 스탯 bgrImg삭제 및 재배치

--- src/components/user/Information/StatInformation.jsx
+++ src/components/user/Information/StatInformation.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import detailBackgrndImg from '../../../assets/ui/basicUi/StatDetailBackgrnd.png';
-import backgrndImg from '../../../assets/ui/basicUi/combatPowerUi.png';
-import attackFont from '../../../assets/ui/basicUi/attackFont.png';
-import mainFont from '../../../assets/ui/basicUi/mainStatFont.png';
-import mainStatUi from '../../../assets/ui/basicUi/mainStat.png';
-import utilityUi from '../../../assets/ui/basicUi/utilityUi.png';
-import utilityFont from '../../../assets/ui/basicUi/utilityFont.png';
+
 
 export const StatInformation = ({ statInfo }) => {
   const formatNumber = (value) => {
@@ -41,154 +35,187 @@ export const StatInformation = ({ statInfo }) => {
 
   return (
     <Container>
-      <Combatpower>
-        <img src={backgrndImg} alt="backgrndImg" />
-        <p>{formatNumber(statInfo.final_stat[42].stat_value)}</p>
-      </Combatpower>
-
-      <StatWrap>
-        <img src={mainStatUi} alt="mainStatUi" />
-        <TextWrap>
-        <TextLeft>
-          <p>{formatNumberWithCommas(statInfo.final_stat[20].stat_value)}</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[16].stat_value)}</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[18].stat_value)}</p>
-        </TextLeft>
-        <TextRight>
-          <p>{formatNumberWithCommas(statInfo.final_stat[21].stat_value)}</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[17].stat_value)}</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[19].stat_value)}</p>
-        </TextRight>
-        </TextWrap>
-        <StatUi>
-          <img src={mainFont} alt="mainFont" />
-        </StatUi>
-      </StatWrap>
-      <AttackFontWrap>
-        <TextLeft>
-          <p>{formatNumber(statInfo.final_stat[1].stat_value)} </p>
-          <p>{statInfo.final_stat[4].stat_value} %</p>
-          <p>{statInfo.final_stat[5].stat_value} %</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[40].stat_value)}</p>
-          <p>{formatNumberWithCommas(statInfo.final_stat[41].stat_value)}</p>
-          <p>{statInfo.final_stat[33].stat_value}초 / {statInfo.final_stat[34].stat_value}%</p>
-          <p>{statInfo.final_stat[35].stat_value} %</p>
-          <p>{statInfo.final_stat[37].stat_value} %</p>
-        </TextLeft>
-        <TextRight>
-          <p>{statInfo.final_stat[2].stat_value} %</p>
-          <p>{statInfo.final_stat[3].stat_value} %</p>
-          <p>{statInfo.final_stat[32].stat_value} %</p>
-          <p>{statInfo.final_stat[6].stat_value} %</p>
-          <p>{statInfo.final_stat[7].stat_value} %</p>
-          <p>{statInfo.final_stat[30].stat_value} %</p>
-          <p>{statInfo.final_stat[36].stat_value} %</p>
-          <p>{statInfo.final_stat[43].stat_value} %</p>
-        </TextRight>
-        <FontUi>
-          <img src={attackFont}alt="attackFont" />
-        </FontUi>
-      </AttackFontWrap>
-      <EtcStatWrap>
-        <img src={utilityUi} alt="utilityUi" />
-        <EtcTextWrap>
+      <AllStatWrap>
+        <CombatpowerWrap>
+            <CombatpowerHeader>
+              {statInfo.final_stat[42].stat_name}
+            </CombatpowerHeader>
+            <p>{formatNumber(statInfo.final_stat[42].stat_value)}</p>
+        </CombatpowerWrap>
+        <StatWrap>
+            <StatLeftWrap>
+              <div>
+                <StatHeader>{statInfo.final_stat[20].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[20].stat_value)}</StatValue>
+              </div>
+              <div>
+                <StatHeader>{statInfo.final_stat[16].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[16].stat_value)}</StatValue>
+              </div>
+              <div>
+                <StatHeader>{statInfo.final_stat[18].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[18].stat_value)}</StatValue>
+              </div>
+            </StatLeftWrap>
+            <StatRightWrap>
+              <div>
+                <StatHeader>{statInfo.final_stat[21].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[21].stat_value)}</StatValue>
+              </div>
+              <div>
+                <StatHeader>{statInfo.final_stat[17].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[17].stat_value)}</StatValue>
+              </div>
+              <div>
+                <StatHeader>{statInfo.final_stat[19].stat_name}</StatHeader>
+                <StatValue>{formatNumberWithCommas(statInfo.final_stat[19].stat_value)}</StatValue>
+              </div>
+            </StatRightWrap>
+        </StatWrap>
+        <AttackFontWrap>
           <TextLeft>
-            <p>{statInfo.final_stat[29].stat_value}%</p>
-            <p>{statInfo.final_stat[28].stat_value}%</p>
-            <p>{statInfo.final_stat[39].stat_value}%</p>
+            <p><StatHeader>스탯 공격력</StatHeader><StatValue>{formatNumber(statInfo.final_stat[1].stat_value)} </StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[4].stat_name}</StatHeader><StatValue>{statInfo.final_stat[4].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[5].stat_name}</StatHeader><StatValue>{statInfo.final_stat[5].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[40].stat_name}</StatHeader><StatValue>{formatNumberWithCommas(statInfo.final_stat[40].stat_value)}</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[41].stat_name}</StatHeader><StatValue>{formatNumberWithCommas(statInfo.final_stat[41].stat_value)}</StatValue></p>
+            <p><StatHeader>재사용 대기시간 감소</StatHeader><StatValue>{statInfo.final_stat[33].stat_value}초 / {statInfo.final_stat[34].stat_value}%</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[35].stat_name}</StatHeader><StatValue>{statInfo.final_stat[35].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[37].stat_name}</StatHeader><StatValue>{statInfo.final_stat[37].stat_value} %</StatValue></p>
           </TextLeft>
           <TextRight>
-            <p>{statInfo.final_stat[13].stat_value}</p>
-            <p>{formatNumberWithCommas(statInfo.final_stat[14].stat_value)}</p>
-            <p>{formatNumberWithCommas(statInfo.final_stat[15].stat_value)}</p>
+            <p><StatHeader>{statInfo.final_stat[2].stat_name}</StatHeader><StatValue>{statInfo.final_stat[2].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[3].stat_name}</StatHeader><StatValue>{statInfo.final_stat[3].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[32].stat_name}</StatHeader><StatValue>{statInfo.final_stat[32].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[6].stat_name}</StatHeader><StatValue>{statInfo.final_stat[6].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[7].stat_name}</StatHeader><StatValue>{statInfo.final_stat[7].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[30].stat_name}</StatHeader><StatValue>{statInfo.final_stat[30].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[36].stat_name}</StatHeader><StatValue>{statInfo.final_stat[36].stat_value} %</StatValue></p>
+            <p><StatHeader>{statInfo.final_stat[43].stat_name}</StatHeader><StatValue>{statInfo.final_stat[43].stat_value} %</StatValue></p>
           </TextRight>
-        </EtcTextWrap>
-        <EtcUi>
-          <img src={utilityFont} alt="utilityFont" />
-        </EtcUi>
-      </EtcStatWrap>
+        </AttackFontWrap>
+        {/* <EtcStatWrap>
+          <EtcTextWrap>
+            <TextLeft>
+              <p>{statInfo.final_stat[29].stat_name}{statInfo.final_stat[29].stat_value}%</p>
+              <p>{statInfo.final_stat[28].stat_name}{statInfo.final_stat[28].stat_value}%</p>
+              <p>{statInfo.final_stat[39].stat_name}{statInfo.final_stat[39].stat_value}%</p>
+            </TextLeft>
+            <TextRight>
+              <p>{statInfo.final_stat[13].stat_name}{statInfo.final_stat[13].stat_value}</p>
+              <p>{statInfo.final_stat[14].stat_name}{formatNumberWithCommas(statInfo.final_stat[14].stat_value)}</p>
+              <p>{statInfo.final_stat[15].stat_name}{formatNumberWithCommas(statInfo.final_stat[15].stat_value)}</p>
+            </TextRight>
+          </EtcTextWrap>
+          <EtcUi>
+          </EtcUi>
+        </EtcStatWrap> */}
+      </AllStatWrap>
     </Container>
   );
 };
 
-const commonTextStyle = `
+
+
+const Container = styled.div`
+  position: relative; 
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+  border: 1px solid rgb(80,92,101);
+  outline: 1px solid rgb(42,49,58);
+  border-radius: 5px;
+  background-color: rgba(59,66,75, 0.9);
+`;
+
+const AllStatWrap = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 5px;
+  padding: 5px;
+`
+
+const CombatpowerWrap = styled.div`
+  color: rgb(255, 250, 210);
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 10px;
+  width: 100%;
+  height: 40px;
+  font-size: 20px;
+  background-color: rgb(62,96,118);
+  border-radius: 5px;
+`;
+
+const CombatpowerHeader = styled.div`
+  width: 20%;
+`
+
+const StatWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  top: 75px;
+  background-color: rgba(134,148,158, 0.8);
+  padding: 8px;
+  gap: 15px;
+  border-radius: 5px;
+`;
+
+const StatLeftWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 53%;
+  div{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 25px;
+  }
+`
+
+const StatRightWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 50%;
+  div{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 25px;
+  }
+`
+
+const StatHeader = styled.div`
+  align-items: flex-start;
+  font-size: 14px;
+  color: white;
+`
+const StatValue = styled.div`
   align-items: flex-end;
   font-size: 14px;
   color: white;
-  gap: 8px;
-`;
-
-const Container = styled.div`
-  width: 472px;
-  height: 479px;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  background-repeat: no-repeat;
-  background-image: url(${detailBackgrndImg});
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
-`;
-
-const Combatpower = styled.div`
-  color: rgb(255, 250, 210);
-  display: flex;
-  justify-content: center;
-  position: absolute;
-  top: 37px;
-  width: 100%;
-  font-size: 20px;
-  p {
-    position: absolute;
-    width: 100%;
-    height: 33px;
-    justify-content: center;
-    display: flex;
-    align-items: center;
-  }
-`;
-
-const StatWrap = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
-  justify-content: flex-end;
-  width: 448px;
-  height: 81px;
-  top: 75px;
-  gap: 170px;
-  img {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-  }
-`;
-
-const StatUi = styled.div`
-  img {
-    width: 242px;
-    height: 53px;
-    position: absolute;
-    left: 13px;
-    top: 13px;
-  }
-`;
+`
 
 const AttackFontWrap = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: flex-end;
-  justify-content: flex-end;
-  gap: 170px;
-  position: absolute;
-  bottom: 131px;
-  left: 12px;
-  width: 448px;
-  height: 190px;
-  padding: 10px 0;
+  justify-content: space-between;
+  padding: 10px 8px;
+  gap: 15px;
+  background-color: rgb(108,120,134);
+  border-radius: 5px;
+  white-space: nowrap;
+  p{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+    gap: 25px;
+  }
 `;
 
 const TextWrap = styled.div`
@@ -204,28 +231,37 @@ const TextWrap = styled.div`
 `;
 
 const TextRight = styled.div`
-  ${commonTextStyle}
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  padding-right: 10px;
+  font-size: 14px;
+  color: white;
+  gap: 8px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  
 `;
 
 const TextLeft = styled.div`
-  ${commonTextStyle}
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  padding-left: 10px;
+  font-size: 14px;
+  color: white;
+  gap: 8px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 `;
 
 const FontUi = styled.div`
-  position: absolute;
   left: 13px;
 `;
 
 const EtcStatWrap = styled.div`
-  position: absolute;
   display: flex;
   width: 448px;
   height: 92px;
@@ -236,7 +272,6 @@ const EtcStatWrap = styled.div`
 const EtcTextWrap = styled.div`
   width: 100%;
   height: 100%;
-  position: absolute;
   display: flex;
   justify-content: flex-end;
   gap: 198px;
@@ -245,11 +280,7 @@ const EtcTextWrap = styled.div`
 `;
 
 const EtcUi = styled.div`
-  img {
-    position: absolute;
-    left: 14px;
-    top: 13px;
-  }
+
 `;
 
 export default StatInformation;
