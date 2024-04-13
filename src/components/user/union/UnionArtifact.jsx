7578c13f47fcd98ca0557bf09220eee9c509488f
LeeYongSoo <sideoff0217@naver.com>
2024-04-14 01:54:59 +0900

    feat: 아티팩트 아이콘 정의

--- src/components/user/union/UnionArtifact.jsx
+++ src/components/user/union/UnionArtifact.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const UnionArtifact = ({ Data }) => {
+  const NameValue = Data.union_artifact_crystal.map(crystal => 
+    crystal.name.replace('크리스탈 : ', '')
+  );
 
+
+  
   return (
-    <div></div>
+    <Container>
+      {Data.union_artifact_crystal.map((crystal, index) => (
+        <InfoWrap key={index}>
+                    <Name>
+                    {NameValue[index]}Lv. {crystal.level}
+          </Name>
+          <Option>
+            <p>{crystal.crystal_option_name_1}</p>
+            <p>{crystal.crystal_option_name_2}</p>
+            <p>{crystal.crystal_option_name_3}</p>
+          </Option>
+
+          <Level>
+
+          </Level>
+      </InfoWrap>
+      ))}
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+  border-radius: 5px;
+  display: flex;
+  gap: 5px;
+  flex-direction: row;
+  font-family: maple-light;
+  background-color: rgb(48,54,63);
+  border-radius: 5px;
+  border: 1px solid rgb(69,89,100);
+  outline: 1px solid rgb(56,70,81);
+  padding: 5px;
+  width: 620px;
+  height: fit-content;
+  flex-wrap: wrap;
+`
+
+
+const InfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(48,54,63);
+  border-radius: 5px;
+  border: 1px solid rgb(136, 184, 212);
+  outline: 1px solid rgb(56,70,81);
+  color: white;
+  gap: 10px;
+  padding: 5px;
+  width: 30%;
+  height: 150px;
+`
+
+
+const Option = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Name = styled.div`
+  
+`
+
+const Level = styled.div`
+`
---
+++ src/components/user/union/UnionArtifactIcon.js
@@ -0,0 +1,66 @@
+import artifect_0_grade1_icon from '../../../assets/union/artifact/artifect_0_grade1_icon.png'
+import artifect_0_grade5_icon from '../../../assets/union/artifact/artifect_0_grade5_icon.png'
+
+import artifect_1_grade1_icon from '../../../assets/union/artifact/artifect_1_grade1_icon.png'
+import artifect_1_grade5_icon from '../../../assets/union/artifact/artifect_1_grade5_icon.png'
+
+import artifect_2_grade1_icon from '../../../assets/union/artifact/artifect_2_grade1_icon.png'
+import artifect_2_grade5_icon from '../../../assets/union/artifact/artifect_2_grade5_icon.png'
+
+import artifect_3_grade1_icon from '../../../assets/union/artifact/artifect_3_grade1_icon.png'
+import artifect_3_grade5_icon from '../../../assets/union/artifact/artifect_3_grade5_icon.png'
+
+import artifect_4_grade1_icon from '../../../assets/union/artifact/artifect_4_grade1_icon.png'
+import artifect_4_grade5_icon from '../../../assets/union/artifact/artifect_4_grade5_icon.png'
+
+import artifect_5_grade1_icon from '../../../assets/union/artifact/artifect_5_grade1_icon.png'
+import artifect_5_grade5_icon from '../../../assets/union/artifact/artifect_5_grade5_icon.png'
+
+import artifect_6_grade1_icon from '../../../assets/union/artifact/artifect_6_grade1_icon.png'
+import artifect_6_grade5_icon from '../../../assets/union/artifact/artifect_6_grade5_icon.png'
+
+import artifect_7_grade1_icon from '../../../assets/union/artifact/artifect_7_grade1_icon.png'
+import artifect_7_grade5_icon from '../../../assets/union/artifact/artifect_7_grade5_icon.png'
+
+import artifect_8_grade1_icon from '../../../assets/union/artifact/artifect_8_grade1_icon.png'
+import artifect_8_grade5_icon from '../../../assets/union/artifact/artifect_8_grade5_icon.png'
+
+const UnionArtifactIcon = {
+  orange_Mushroom: [
+    artifect_0_grade1_icon,
+    artifect_0_grade5_icon
+  ],
+  slime: [
+    artifect_1_grade1_icon,
+    artifect_1_grade5_icon
+  ],
+  horny_Mushroom: [
+    artifect_2_grade1_icon,
+    artifect_2_grade5_icon
+  ],
+  stump: [
+    artifect_3_grade1_icon,
+    artifect_3_grade5_icon
+  ],
+  stone_Golem: [
+    artifect_4_grade1_icon,
+    artifect_4_grade5_icon
+  ],
+  balrog: [
+    artifect_5_grade1_icon,
+    artifect_5_grade5_icon
+  ],
+  zaqqum: [
+    artifect_6_grade1_icon,
+    artifect_6_grade5_icon
+  ],
+  pink_Bean: [
+    artifect_7_grade1_icon,
+    artifect_7_grade5_icon
+  ],
+  papulatus: [
+    artifect_8_grade1_icon,
+    artifect_8_grade5_icon
+  ]
+}
+export default UnionArtifactIcon;
