011da7cdddbac7506246e3298360fc697987da60
LeeYongSoo <sideoff0217@naver.com>
2024-05-23 22:03:38 +0900

    feat: 미사용 코드 삭제

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -13,15 +13,14 @@ export const Footer = () => {
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
   height: 70px;
   width: 100%;
+  font-family: maple-light, Arial, sans-serif;
 `;
 
 const FooterTextDiv = styled.div`
-  font-family: 'maple-light', Arial, sans-serif;
   white-space: pre-line; 
 `;
