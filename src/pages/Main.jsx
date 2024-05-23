c64c2a2f2110ab2f61c284dbf3356960a119e3d1
LeeYongSoo <sideoff0217@naver.com>
2024-05-24 01:03:01 +0900

    feat:즐겨찾기 컴포넌트 키 조정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Search } from '../components/main/Search'
 import styled from 'styled-components'
-
+import { Footer } from '../components/common/Footer'
 import { Favorite } from '../components/user/favorite/Favorite'
 
 export const Main = () => {
@@ -9,8 +9,13 @@ export const Main = () => {
     <Container>
       <SearchWrap>
         <Search />
+        <FavoriteWrap>
+          <Favorite/>
+        </FavoriteWrap>
       </SearchWrap>
-
+      <FooterWrap>
+        <Footer />
+      </FooterWrap>
     </Container>
   )
 }
@@ -35,6 +40,16 @@ const SearchWrap = styled.div`
   z-index: 9999;
 `
 
+
+const FooterWrap = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  z-index: 999;
+  @media screen and (max-width:768px){
+    display: block;
+  }
+`
 
 const FavoriteWrap = styled.div`
   position: absolute;
