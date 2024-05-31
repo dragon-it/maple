ebad79e5c8c4a173858191e22c452f598b350b90
LeeYongSoo <sideoff0217@naver.com>
2024-05-31 22:47:56 +0900

    feat: footer dom 최소

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -13,11 +13,11 @@ export const Footer = () => {
 };
 
 const FooterTextSpan = styled.span`
-  white-space: pre-line; 
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
   height: 70px;
   font-family: maple-light;
+  z-index: 9999;
 `;
