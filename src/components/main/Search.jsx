bf2b6a1b43a488e78d525d1005d8c25574168672
LeeYongSoo <sideoff0217@naver.com>
2024-05-31 17:24:28 +0900

    feat: css분리(test)

--- src/components/main/Search.jsx
+++ src/components/main/Search.jsx
@@ -18,35 +18,37 @@ export const Search = ({ error }) => {
     const processedSearchValue = searchValue.replace(/\s+/g, '');
     navigate(`/user/${encodeURIComponent(processedSearchValue)}`);
   };
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
     handleSearch();
   };
 
-  const UserRoute = location.pathname.startsWith('/user/');
+  const isUserRoute = location.pathname.startsWith('/user/');
 
   return (
     <Container>
       <form onSubmit={handleSubmit}>
-        <InputContainer UserRoute={UserRoute}>
-          <Logo error={error}/>
+        <InputContainer isUserRoute={isUserRoute}>
+          <Logo 
+          error={error}
+          isUserRoute={isUserRoute}
+          />
           <InputWrap>
             <StyledInput
               type="text"
               placeholder="캐릭터 닉네임을 입력해주세요."
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
-              UserRoute={UserRoute}
+              isUserRoute={isUserRoute}
             />
-            <StyledButton UserRoute={UserRoute}>
-            <img 
-              src={UserRoute ? serchIcon_small : serchIcon_big} 
-              alt="검색" 
-              width={UserRoute ? "18" : "23"} 
-              height={UserRoute ? "18" : "23"}
-            />
+            <StyledButton isUserRoute={isUserRoute}>
+              <img 
+                src={isUserRoute ? serchIcon_small : serchIcon_big} 
+                alt="검색" 
+                width={isUserRoute ? "18" : "23"} 
+                height={isUserRoute ? "18" : "23"}
+              />
             </StyledButton>
           </InputWrap>
         </InputContainer>
@@ -58,9 +60,7 @@ export const Search = ({ error }) => {
   );
 };
 
-const Container = styled.div`
-
-`;
+const Container = styled.div``;
 
 const InputContainer = styled.div`
   display: flex;
@@ -69,44 +69,43 @@ const InputContainer = styled.div`
   @media screen and (max-width:1024px) {
     flex-direction: column;
   }
-
 `;
-
 
 const InputWrap = styled.div`
   position: relative;
   display: flex;
   flex-direction: row;
   align-items: center;
-`
+`;
 
 const StyledInput = styled.input`
   display: flex;
   position: relative;
   justify-content: center;
   align-items: center;
-  width: ${({ UserRoute }) => (UserRoute ? '210px' : '240px')};
-  height: ${({ UserRoute }) => (UserRoute ? '25px' : '35px')};
+  width: ${({ isUserRoute }) => (isUserRoute ? '210px' : '240px')};
+  height: ${({ isUserRoute }) => (isUserRoute ? '25px' : '35px')};
   padding: 2px 10px;
   border: 2px solid rgba(0, 0, 0, 0.9);
   border-radius: 7px;
   outline: none;
   font-size: 13px;
   background-color: rgba(255, 255, 255, 0.4);
+
   &:focus {
     border-color: rgba(255, 51, 0, 0.9);
     box-shadow: 0 0 10px rgba(255, 51, 0, 0.5);
   }
-  &:focus + button {
-    color: rgba(255, 51, 0, 0.9);    
-  };
 
+  &:focus + button {
+    color: rgba(255, 51, 0, 0.9);
+  }
 `;
 
 const StyledButton = styled.button`
   position: absolute;
   right: 10px;
-  width: ${({ UserRoute }) => (UserRoute ? '30px' : '35px')};
+  width: ${({ isUserRoute }) => (isUserRoute ? '30px' : '35px')};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -125,5 +124,4 @@ const ThemeToggleWrap = styled.div`
     right: 10px;
     top: 5px;
   }
-
-`
+`;
--- src/components/theme/GlobalStyles.js
+++ src/components/theme/GlobalStyles.js
@@ -5,22 +5,5 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.bgColor};
     color: ${({ theme }) => theme.textColor};
-    width: 100%;
-    height: 100%;
-    line-height: 1.2;
-    margin: 0 auto;
-    font-family:
-      sans-serif,
-      maple-light,
-      "Montserrat",
-      "Helvetica Neue",
-      "NanumSquare",
-      "Noto Sans",
-      "Noto Sans CJK KR",
-      maple-bold;
-    word-break: keep-all;
-    word-wrap: break-word;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
   }
 `;
