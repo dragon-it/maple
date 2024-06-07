b0425a9f5c5c83e5610c8afcc01a69edcfe50aa8
LeeYongSoo <sideoff0217@naver.com>
2024-06-07 23:52:55 +0900

    feat: 길드 마스터, 명성치, 레벨, 멤버수 출력

--- src/components/user/guild/GuildInformation.jsx
+++ src/components/user/guild/GuildInformation.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 export const GuildInformation = ({ result }) => {
-  console.log(result);
-  return <div>{result.guild_master_name}</div>;
+  console.log(result.guildBasicInformation);
+  return (
+    <>
+      <GuildMaster>
+        길드 마스터 : {result.guildBasicInformation.guild_master_name}
+      </GuildMaster>
+      <GuildFame>
+        길드 명성치 : {result.guildBasicInformation.guild_fame}
+      </GuildFame>
+      <GuildLevel>
+        길드 레벨 : {result.guildBasicInformation.guild_level}
+      </GuildLevel>
+      <GuildMemberCount>
+        멤버수 : {result.guildBasicInformation.guild_member_count}명
+      </GuildMemberCount>
+    </>
+  );
 };
+
+const GuildMaster = styled.div``;
+
+const GuildFame = styled.div``;
+
+const GuildLevel = styled.div``;
+
+const GuildMemberCount = styled.div``;
