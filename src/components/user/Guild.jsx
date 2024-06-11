7c105c1ddd777d3fdad5588f355d78fa6bc9bdd1
LeeYongSoo <sideoff0217@naver.com>
2024-06-12 03:47:08 +0900

    feat: 길드 마크 출력

--- src/components/user/Guild.jsx
+++ src/components/user/Guild.jsx
@@ -10,11 +10,29 @@ export const Guild = ({ result }) => {
     setActiveTab(tabNumber);
   };
   console.log(result.guildBasicInformation);
+  const guildInfo = result.guildBasicInformation;
 
   return (
     <Container>
-      <GuildHeader>Guild</GuildHeader>
       <TabWrap>
+        <GuildHeader>길드 Guild</GuildHeader>
+        <GuildBasicInformation>
+          <Icon>
+            {guildInfo.guild_mark ? (
+              <img
+                src={`data:image/png;base64,${guildInfo.guild_mark}`}
+                alt="GuildIcon"
+              />
+            ) : (
+              <img
+                src={`data:image/png;base64,${guildInfo.guild_mark_custom}`}
+                alt="GuildIcon"
+              />
+            )}
+          </Icon>
+          <Name>{guildInfo.guild_name}</Name>
+          <Level>Lv.{guildInfo.guild_level}</Level>
+        </GuildBasicInformation>
         <Tab onClick={() => handleTabClick(1)} active={activeTab === 1}>
           길드 정보
         </Tab>
@@ -25,7 +43,6 @@ export const Guild = ({ result }) => {
           길드 스킬
         </Tab>
       </TabWrap>
-
       {activeTab === 1 && <GuildInformation result={result} />}
       {activeTab === 2 && <GuildMember result={result} />}
       {activeTab === 3 && <GuildSkill result={result} />}
@@ -49,7 +66,27 @@ const GuildHeader = styled.div`
   text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
 `;
 
-const Tab = styled.div`
+const GuildBasicInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: maple-light;
+`;
+
+const Icon = styled.div`
+  width: auto;
+  color: aliceblue;
+  img {
+    min-width: 40px;
+  }
+`;
+
+const Name = styled.span``;
+
+const Level = styled.span``;
+
+const Tab = styled.span`
   background-color: red;
 `;
 
