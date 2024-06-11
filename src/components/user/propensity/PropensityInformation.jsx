7493986278d7ff4abeb1710f0a7d0b7d44c483a3
LeeYongSoo <sideoff0217@naver.com>
2024-06-11 23:55:13 +0900

    feat: 즐겨찾기 텍스트 위치 조정

--- src/components/user/propensity/PropensityInformation.jsx
+++ src/components/user/propensity/PropensityInformation.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, Tooltip } from 'recharts';
+import React, { useState } from "react";
+import styled from "styled-components";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  Tooltip,
+} from "recharts";
 
 export const PropensityInformation = ({ propensityData }) => {
   const [tooltipVisible, setTooltipVisible] = useState(true);
   const data = [
-    { subject: '카리스마', A: propensityData.charisma_level },
-    { subject: '매력', A: propensityData.charm_level },
-    { subject: '손재주', A: propensityData.handicraft_level },
-    { subject: '통찰력', A: propensityData.insight_level },
-    { subject: '감성', A: propensityData.sensibility_level },
-    { subject: '의지', A: propensityData.willingness_level },
+    { subject: "카리스마", A: propensityData.charisma_level },
+    { subject: "매력", A: propensityData.charm_level },
+    { subject: "손재주", A: propensityData.handicraft_level },
+    { subject: "통찰력", A: propensityData.insight_level },
+    { subject: "감성", A: propensityData.sensibility_level },
+    { subject: "의지", A: propensityData.willingness_level },
   ];
 
   const PropensityItem = ({ label, level }) => (
@@ -31,45 +37,67 @@ export const PropensityInformation = ({ propensityData }) => {
       <PropensityHeader>PROPENSITY</PropensityHeader>
       <PropensityTextWrap>
         <TextWrap>
-            <PropensityItem label="카리스마" level={propensityData.charisma_level} />
-            <PropensityItem label="감성" level={propensityData.sensibility_level} />
-            <PropensityItem label="통찰력" level={propensityData.insight_level} />
-          </TextWrap>
-          <TextWrap>     
-            <PropensityItem label="의지" level={propensityData.willingness_level} />
-            <PropensityItem label="손재주" level={propensityData.handicraft_level} />
-            <PropensityItem label="매력" level={propensityData.charm_level} />
-          </TextWrap>
+          <PropensityItem
+            label="카리스마"
+            level={propensityData.charisma_level}
+          />
+          <PropensityItem
+            label="감성"
+            level={propensityData.sensibility_level}
+          />
+          <PropensityItem label="통찰력" level={propensityData.insight_level} />
+        </TextWrap>
+        <TextWrap>
+          <PropensityItem
+            label="의지"
+            level={propensityData.willingness_level}
+          />
+          <PropensityItem
+            label="손재주"
+            level={propensityData.handicraft_level}
+          />
+          <PropensityItem label="매력" level={propensityData.charm_level} />
+        </TextWrap>
       </PropensityTextWrap>
       <ChartWrap>
-      <RadarChart
+        <RadarChart
           cx={152}
           cy={115}
           outerRadius={100}
           width={320}
           height={250}
           data={data}
-          onClick={handleChartClick}>
+          onClick={handleChartClick}
+        >
           <PolarGrid />
-          <Tooltip formatter={(value) => `레벨: ${value}`}  active={tooltipVisible} />
+          <Tooltip
+            formatter={(value) => `레벨: ${value}`}
+            active={tooltipVisible}
+          />
           <PolarAngleAxis dataKey="subject" display="none" />
-          <Radar name="레벨" dataKey="A" stroke="#3498db" fill="#3498db" fillOpacity={0.6} />
+          <Radar
+            name="레벨"
+            dataKey="A"
+            stroke="#3498db"
+            fill="#3498db"
+            fillOpacity={0.6}
+          />
         </RadarChart>
         <ItemsWrap>
-            <SubjectItems>
-              <p>의지</p>
-              <p>감성</p>
-            </SubjectItems>
-            <SubjectItems>
-              <p>카리스마</p>
-              <p>통찰력</p>
-            </SubjectItems>
-            <SubjectItems>
-              <p>매력</p>
-              <p>손재주</p>
-            </SubjectItems>
-          </ItemsWrap>
-        </ChartWrap>
+          <SubjectItems>
+            <p>의지</p>
+            <p>감성</p>
+          </SubjectItems>
+          <SubjectItems>
+            <p>카리스마</p>
+            <p>통찰력</p>
+          </SubjectItems>
+          <SubjectItems>
+            <p>매력</p>
+            <p>손재주</p>
+          </SubjectItems>
+        </ItemsWrap>
+      </ChartWrap>
     </Container>
   );
 };
@@ -80,28 +108,28 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 99;
-  border: 1px solid rgb(80,92,101);
-  outline: 1px solid rgb(42,49,58);
+  border: 1px solid rgb(80, 92, 101);
+  outline: 1px solid rgb(42, 49, 58);
   width: 100%;
   border-radius: 5px;
-  background-color: rgba(59,66,75, 0.9);
+  background-color: rgba(59, 66, 75, 0.9);
   padding: 5px;
   color: black;
 
-  @media screen and (max-width:576px) {
+  @media screen and (max-width: 576px) {
     width: 100%;
   }
 `;
 
 const PropensityItemWrapper = styled.p`
-  background-color: rgb(31,189,255);
+  background-color: rgb(31, 189, 255);
   width: 120px;
 
-  @media screen and (max-width:576px) {
+  @media screen and (max-width: 576px) {
     width: 105px;
   }
 
-  @media screen and (max-width:376px) {
+  @media screen and (max-width: 376px) {
     width: 95px;
   }
 `;
@@ -110,44 +138,42 @@ const PropensityHeader = styled.div`
   width: 100%;
   font-size: 15px;
   font-weight: 700;
-  color: rgb(220,252,2);
+  color: rgb(220, 252, 2);
   margin-bottom: 5px;
   text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
-`
+`;
 
 const PropensityTextWrap = styled.div`
-margin-bottom: 10px;
-background-color: #ffffff;
-border-radius: 5px;
-padding: 2px;
-`
+  margin-bottom: 10px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  padding: 2px;
+`;
 
 const TextWrap = styled.div`
   display: flex;
   flex-direction: row;
-  p{
+  p {
     display: flex;
     margin: 2px;
     justify-content: space-between;
   }
-`
+`;
 
 const SubjectItems = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  p{
+  p {
     padding: 6px;
     background-color: rgb(106, 214, 241);
     color: white;
     font-weight: 700;
     text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
     border-radius: 5px;
-    
   }
-
-`
+`;
 const ItemsWrap = styled.div`
   display: flex;
   position: absolute;
@@ -158,24 +184,24 @@ const ItemsWrap = styled.div`
   gap: 60px;
   top: -10px;
   z-index: -1;
-  :nth-child(1){
+  :nth-child(1) {
     gap: 80px;
   }
-  :nth-child(2){
+  :nth-child(2) {
     gap: 220px;
   }
-  :nth-child(3){
+  :nth-child(3) {
     gap: 80px;
     align-items: flex-start;
   }
-`
+`;
 const ChartWrap = styled.div`
   position: relative;
   margin-top: 40px;
   margin-bottom: 20px;
   width: 320px;
   z-index: 99;
-`
+`;
 const PropensityName = styled.div`
   display: flex;
   flex-direction: column;
@@ -183,33 +209,33 @@ const PropensityName = styled.div`
   justify-content: center;
   white-space: nowrap;
   font-size: 13px;
-  p{
+  p {
     padding: 5px;
     background-color: rgb(106, 214, 241);
     color: white;
     font-weight: 700;
     text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+    border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 5px;
   }
-  @media screen and (max-width:576px) {
+  @media screen and (max-width: 576px) {
     width: 100%;
   }
 
-  @media screen and (max-width:376px) {
+  @media screen and (max-width: 376px) {
     font-size: 12px;
   }
-`
+`;
 
 const PropenLabel = styled.div`
   padding-right: 5px;
-`
+`;
 const PropenLevel = styled.div`
   width: 40px;
   display: flex;
   justify-content: flex-start;
 
-  @media screen and (max-width:576px) {
+  @media screen and (max-width: 576px) {
     width: auto;
   }
-
-`
+`;
