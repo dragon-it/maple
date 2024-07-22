06a0e50db3a062e1c11a84dd918e792e9114fd46
dragon <sideoff0217@naver.com>
2024-07-22 16:48:19 +0900

    fix: 길드 없을 시 출력 메세지 컨테이너 디자인 수정

--- src/components/user/guild/GuildInformation.jsx
+++ src/components/user/guild/GuildInformation.jsx
@@ -2,27 +2,41 @@ import React from "react";
 import styled from "styled-components";
 
 export const GuildInformation = ({ result }) => {
-  const { guild_master_name, guild_fame, guild_point, guild_member_count } =
+  const { guild_master_name, guild_fame, guild_point, guild_member_count,guild_name } =
     result.guildBasicInformation;
 
   return (
     <Container>
-      <Master>길드 마스터 : {guild_master_name}</Master>
-      <Fame>길드 명성치 : {guild_fame.toLocaleString()}</Fame>
-      <Point>길드 포인트 : {guild_point.toLocaleString()}</Point>
-      <MemberCount>
-        길드 인원수 : {guild_member_count.toLocaleString()}명
-      </MemberCount>
+      <Items>
+        <InfoHeader>길드명</InfoHeader>{guild_name}
+      </Items>
+      <Items>
+        <InfoHeader>명성치</InfoHeader>
+        {guild_fame.toLocaleString()}
+      </Items>
+      <Items>
+        <InfoHeader>길드 포인트</InfoHeader>
+        {guild_point.toLocaleString()}
+      </Items>
+      <Items>
+        <InfoHeader>길드 인원수</InfoHeader>
+        {guild_member_count.toLocaleString()}명
+      </Items>
+      <Items>
+        <InfoHeader>길드 마스터</InfoHeader>
+        {guild_master_name}
+      </Items>
     </Container>
   );
 };
 
 const Container = styled.div``;
 
-const Master = styled.div``;
+const Items = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 
-const Fame = styled.div``;
-
-const Point = styled.div``;
-
-const MemberCount = styled.div``;
+const InfoHeader = styled.div`
+  
+`
