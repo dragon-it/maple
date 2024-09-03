72aa4eecd53724f0ef426ce29da9da64ccc83245
LeeYongSoo <sideoff0217@naver.com>
2024-09-04 02:13:54 +0900

    feat: 메뉴 버튼 추가 및 메뉴판 렌더링

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -13,28 +13,55 @@ export const Menu = () => {
   };
 
   return (
-    <Container>
-      <HamburgerImg
-        onClick={handleClicked}
-        src={theme === "light" ? hamburger_bar_dark : hamburger_bar_light}
-        alt="hamburger_bar"
-      ></HamburgerImg>
-    </Container>
+    <>
+      <Container>
+        <HamburgerImg
+          onClick={handleClicked}
+          src={theme === "light" ? hamburger_bar_dark : hamburger_bar_light}
+          alt="hamburger_bar"
+        />
+      </Container>
+      <MenuContainer isClicked={isClicked}>
+        <p>Menu</p>
+        <MenuCloseBtn onClick={handleClicked}></MenuCloseBtn>
+      </MenuContainer>
+    </>
   );
 };
 
 const Container = styled.div`
   position: absolute;
-  right: 0;
-  width: 22px;
-  height: 22px;
+  right: 24px;
+  top: 24px;
 `;
 
 const HamburgerImg = styled.img`
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 22px;
-  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid rgb(0, 0, 0);
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.15);
   cursor: pointer;
+
+  &:hover {
+    background-color: rgba(56, 56, 56, 0.65);
+  }
 `;
+
+const MenuContainer = styled.div`
+  position: fixed;
+  top: 0;
+  right: ${({ isClicked }) => (isClicked ? "0" : "-100%")};
+  width: 300px;
+  height: 100%;
+  background-color: #222;
+  transition: right 0.3s ease;
+  z-index: 1000;
+  padding: 20px;
+  box-shadow: ${({ isClicked }) =>
+    isClicked ? "0px 0px 10px rgba(0, 0, 0, 0.5)" : "none"};
+`;
+
+const MenuCloseBtn = styled.button``;
