2ee4974896be64134673f0be1c1efe73bd932f15
LeeYongSoo <sideoff0217@naver.com>
2024-09-03 23:07:36 +0900

    feat: 햄버거 바 테마별 이미지 출력

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import hamburger_bar_dark from "../../../assets/menu/hamburger_dark.svg";
+import hamburger_bar_light from "../../../assets/menu/hamburger_light.svg";
+import { useTheme } from "../../../context/ThemeProvider";
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  const { theme } = useTheme();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClicked = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <Container>
+      <HamburgerImg
+        onClick={handleClicked}
+        src={theme === "light" ? hamburger_bar_dark : hamburger_bar_light}
+        alt="hamburger_bar"
+      ></HamburgerImg>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  position: absolute;
+  right: 0;
+  width: 22px;
+  height: 22px;
+`;
+
+const HamburgerImg = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 22px;
+  height: auto;
+  cursor: pointer;
+`;
