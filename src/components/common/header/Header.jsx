b1c31ac8667fda1e5e1f145d2ec455335cc52b2a
LeeYongSoo <sideoff0217@naver.com>
2024-09-07 23:18:57 +0900

    feat: 반응형 헤더 출력 설정, 배경화면 밀림 해결

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+import { Menu } from "../menu/Menu";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <PcHeaderContainer>
+        <HeaderLogo alt="" />
+        메짱 로고
+      </PcHeaderContainer>
+
+      <Menu />
+    </>
+  );
 };
+
+const PcHeaderContainer = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 40px;
+  background-color: #000000;
+
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const HeaderLogo = styled.img``;
