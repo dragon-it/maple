a0abf88160413a9516569355f5dd3e026f367395
LeeYongSoo <sideoff0217@naver.com>
2024-09-13 20:18:02 +0900

    fix: port 변경 배포테스트

--- proxyServer.js
+++ proxyServer.js
@@ -7,7 +7,7 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const BASE_URL = "https://open.api.nexon.com";
 
 app.use(express.json());
