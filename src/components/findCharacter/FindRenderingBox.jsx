e9ad23333c229fc6fa536158a10c37b07ebdfefc
dragon <sideoff0217@naver.com>
2024-09-26 20:20:42 +0900

    feat: 본캐 찾기 닉네임 디자인 완료

--- src/components/findCharacter/FindRenderingBox.jsx
+++ src/components/findCharacter/FindRenderingBox.jsx
@@ -5,43 +5,35 @@ import { PowerStat } from "../common/powerStat/PowerStat";
 import npc_Chat_Box from "../../assets/npc/npc_Chat_Box.png";
 
 const StyledLine = styled.p`
-  font-size: 16px; /* 기본 글꼴 크기 */
-  margin: 5px 0; /* 위아래 여백 */
+  font-size: 15px; 
+  margin: 5px 0; 
+  color: rgb(0,0,0);
 `;
 
 const LineStyle1 = styled(StyledLine)`
-  color: #333;
-  background-color: #f0f8ff;
-  padding: 5px;
-  border-radius: 5px;
+
+
+
 `;
 
 const LineStyle2 = styled(StyledLine)`
-  color: #fff;
-  background-color: #4caf50;
-  padding: 5px;
-  border-radius: 5px;
+
+
 `;
 
 const LineStyle3 = styled(StyledLine)`
-  color: #000;
-  background-color: #ff9800;
-  padding: 5px;
-  border-radius: 5px;
+
+
 `;
 
 const LineStyle4 = styled(StyledLine)`
-  color: #000;
-  background-color: #2196f3;
-  padding: 5px;
-  border-radius: 5px;
+
+
 `;
 
 const LineStyle5 = styled(StyledLine)`
-  color: #fff;
-  background-color: #9c27b0;
-  padding: 5px;
-  border-radius: 5px;
+
+
 `;
 
 const lineStyles = [LineStyle1, LineStyle2, LineStyle3, LineStyle4, LineStyle5];
@@ -99,10 +91,11 @@ export const FindRenderingBox = ({ result }) => {
 
   const characterInfoLines = [
     `닉네임: ${character_name || "이름 없음"}`,
-    `직업: ${character_class || "직업 없음"}`,
-    `길드: ${character_guild_name || "길드 없음"}`,
     `레벨: ${character_level || "레벨 정보 없음"}`,
+    `직업: ${character_class || "직업 없음"}`,
     `월드: ${world_name || "월드 정보 없음"}`,
+    `길드: ${character_guild_name || "길드 없음"}`,
+
   ];
 
   return (
@@ -113,7 +106,10 @@ export const FindRenderingBox = ({ result }) => {
       <MainCharacterWrap>
         <NpcBox src={npc_Chat_Box} alt="대화박스" />
         <CharacterInfo>
-          <Image src={character_image} alt="캐릭터 이미지" />
+          <NpcWrap>
+            <Image src={character_image} alt="캐릭터 이미지" />
+            <NickName>{character_name}</NickName>
+          </NpcWrap>
           <LineTypingEffect lines={characterInfoLines} speed={50} />
           <PowerStat powerValue={powerValue} />
         </CharacterInfo>
@@ -122,9 +118,10 @@ export const FindRenderingBox = ({ result }) => {
   );
 };
 
-// 스타일 정의
+
 const Container = styled.div`
   width: 100%;
+  height: 100%;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -133,7 +130,6 @@ const Container = styled.div`
 `;
 
 const MainCharacterWrap = styled.div`
-  background-color: aliceblue;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
@@ -146,21 +142,56 @@ const Image = styled.img`
   width: 100px;
   height: 100px;
   object-fit: cover;
-  border-radius: 50%;
-  margin-bottom: 10px;
   z-index: 10;
+  transform: scaleX(-1);
+  position: relative;
+  top: 7px;
 `;
 
 const NpcBox = styled.img`
-  position: relative; /* 이미지 자체는 위치 설정 가능 */
+  position: relative; 
   z-index: 1;
   width: fit-content;
   height: auto;
+  
 `;
 
 const CharacterInfo = styled.div`
-  position: absolute; /* NpcBox 안에서 캐릭터 정보 위치 조정 */
-  top: 20%; /* 필요에 따라 조정 */
-  left: 10%; /* 필요에 따라 조정 */
-  z-index: 10; /* 이미지 위에 표시되도록 */
+  display: flex;
+  gap: 40px;
+  position: absolute;
+  flex-direction: row;
+  z-index: 10; 
+  max-width: 519px;
+  max-height: 202px;
+  transform: translateX(-3%) translateY(20%);
 `;
+
+const NickName = styled.div`
+  position: relative;
+  background: linear-gradient(180deg, rgba(150,149,143,1) 0%, rgba(136,136,136,1) 49%, rgba(108,106,106,1) 100%);
+  color: rgb(247, 247, 247);
+  border: 1px solid rgb(82, 79, 87);
+  border-radius: 7px;
+  text-align: center;
+
+  &::after {
+    content: '';
+    display: inline-block;
+    background: rgb(230, 230, 230);
+    padding: 2px;
+    border: 1px solid #494949;
+    position: absolute;
+    bottom: -3px;
+    left: -3px;
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    z-index: -1;
+  }
+`
+
+const NpcWrap = styled.div`
+  
+
+`
