df23f6e42ead9fb96e399bccd96ea6b0aef62438
LeeYongSoo <sideoff0217@naver.com>
2024-10-11 01:37:06 +0900

    feat: 길드 검색 후 길드 클릭시 라우트 이동

--- src/components/searchGuild/SearchGuildRendering.jsx
+++ src/components/searchGuild/SearchGuildRendering.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 export const SearchGuildRendering = ({ result }) => {
   const { guildName } = useParams();
+  const navigate = useNavigate();
+
+  const handleClick = (world_name) => {
+    navigate(`/guild-search/${guildName}/${world_name}`);
+  };
+
   return (
     <Container>
       {result && result.length > 0 ? (
         result.map(({ world_name, oguild_id }) => (
-          <GuildItem key={oguild_id}>
+          <GuildItem key={oguild_id} onClick={() => handleClick(world_name)}>
             <p>{world_name} </p>
             <div>{guildName}</div>
           </GuildItem>
@@ -30,4 +36,6 @@ const GuildItem = styled.div`
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  cursor: pointer;
+  background-color: red;
 `;
