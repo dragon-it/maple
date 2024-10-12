fbe9c6cce4dd6880dd96852c173046ce40c62649
LeeYongSoo <sideoff0217@naver.com>
2024-10-12 17:31:16 +0900

    feat: 길드 찾기 api 연결 완료

--- src/components/searchGuild/SearchGuildRendering.jsx
+++ src/components/searchGuild/SearchGuildRendering.jsx
@@ -1,26 +1,45 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { React, useState } from "react";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 import styled from "styled-components";
+import { SearchGuildDetail } from "./SearchGuildDetail";
 
 export const SearchGuildRendering = ({ result }) => {
   const { guildName } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedGuild, setSelectedGuild] = useState({
+    oguild_id: null,
+    world_name: "",
+  });
 
-  const handleClick = (world_name) => {
+  const handleClick = (oguild_id, world_name) => {
+    setSelectedGuild({ oguild_id, world_name });
     navigate(`/guild-search/${guildName}/${world_name}`);
   };
 
+  // 현재 URL과 선택된 world_name을 비교
+  const isGuildDetail = location.pathname.startsWith(
+    `/guild-search/${encodeURIComponent(guildName)}/${encodeURIComponent(
+      selectedGuild.world_name
+    )}`
+  );
+
   return (
     <Container>
-      {result && result.length > 0 ? (
-        result.map(({ world_name, oguild_id }) => (
-          <GuildItem key={oguild_id} onClick={() => handleClick(world_name)}>
-            <p>{world_name} </p>
+      {isGuildDetail ? (
+        <SearchGuildDetail selectedGuild={selectedGuild} />
+      ) : result && result.length > 0 ? (
+        result.map(({ oguild_id, world_name }) => (
+          <GuildItem
+            key={oguild_id}
+            onClick={() => handleClick(oguild_id, world_name)}
+          >
+            <p>{world_name}</p>
             <div>{guildName}</div>
           </GuildItem>
         ))
       ) : (
-        <></>
+        <div>검색 결과가 없습니다.</div>
       )}
     </Container>
   );
@@ -37,5 +56,4 @@ const GuildItem = styled.div`
   border: 1px solid #ccc;
   border-radius: 5px;
   cursor: pointer;
-  background-color: red;
 `;
