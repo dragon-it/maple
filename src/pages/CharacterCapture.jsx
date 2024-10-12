5e622b5f92be05d65247f3ee62dfabe22ac10ec9
LeeYongSoo <sideoff0217@naver.com>
2024-10-12 23:46:43 +0900

    feat: 검색 버튼 디자인 완성

--- src/pages/CharacterCapture.jsx
+++ src/pages/CharacterCapture.jsx
@@ -91,6 +91,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 10px;
 
   @media screen and (max-width: 768px) {
     padding: 0 5px;
