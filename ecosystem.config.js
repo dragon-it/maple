c281c2b5b16d4f64dd528e490b7c3a403d694f1b
dragon-it <sideoff0217@naver.com>
2025-02-27 09:02:06 +0000

    무중단 배포 코드 merge

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,36 @@
 module.exports = {
   apps: [
     {
-      name: 'mezzang-blue',  // 첫 번째 서버 이름
+      name: 'mezzang-blue',
       script: './proxyServer.js',
-      instances: 1,             // 단일 프로세스
-      exec_mode: 'fork',        // 포크 모드
+      instances: 1,  
+      exec_mode: 'fork', // 포크 모드
       env: {
-        PORT: 3001,             // 첫 번째 서버가 사용할 포트
-        BUILD_DIR: 'build-blue', // 첫 번째 서버의 빌드 폴더
+        PORT: 3001,
+        BUILD_DIR: 'build-blue',
       },
-      watch: false,             // 파일 변경 감지 안 함
-      autorestart: true,        // 오류 발생 시 재시작
+      env_production: {
+        PORT: 3001,  // 프로덕션 환경에서 사용할 포트
+        BUILD_DIR: 'build-blue',
+      },
+      watch: false,
+      autorestart: true,
     },
     {
-      name: 'mezzang-green',      // 두 번째 서버 이름
+      name: 'mezzang-green',
       script: './proxyServer.js',
-      instances: 1,             // 단일 프로세스
-      exec_mode: 'fork',        // 포크 모드
+      instances: 1,
+      exec_mode: 'fork',
       env: {
-        PORT: 3002,             // 두 번째 서버가 사용할 포트
-        BUILD_DIR: 'build-green', // 두 번째 서버의 빌드 폴더
+        PORT: 3002,
+        BUILD_DIR: 'build-green',
       },
-      watch: false,             // 파일 변경 감지 안 함
-      autorestart: true,        // 오류 발생 시 재시작
+      env_production: {
+        PORT: 3002,  // 프로덕션 환경에서 사용할 포트
+        BUILD_DIR: 'build-green',
+      },
+      watch: false,
+      autorestart: true,
     },
   ],
 
