cd7df3932b3d2ae6b4e2900b98c9349d8603c008
LeeYongSoo <sideoff0217@naver.com>
2025-03-07 22:36:56 +0900

    feat: 아트웍 선택 기능, 아트웍 파일 위치 변경

---
+++ src/components/slidingPuzzle/SlidingPuzzleImages.js
@@ -0,0 +1,70 @@
+import artwork1_1 from "../../assets/slidingPuzzle/3x3/artwork1/1.jpg";
+import artwork1_2 from "../../assets/slidingPuzzle/3x3/artwork1/2.jpg";
+import artwork1_3 from "../../assets/slidingPuzzle/3x3/artwork1/3.jpg";
+import artwork1_4 from "../../assets/slidingPuzzle/3x3/artwork1/4.jpg";
+import artwork1_5 from "../../assets/slidingPuzzle/3x3/artwork1/5.jpg";
+import artwork1_6 from "../../assets/slidingPuzzle/3x3/artwork1/6.jpg";
+import artwork1_7 from "../../assets/slidingPuzzle/3x3/artwork1/7.jpg";
+import artwork1_8 from "../../assets/slidingPuzzle/3x3/artwork1/8.jpg";
+import artwork1_9 from "../../assets/slidingPuzzle/3x3/artwork1/9.jpg";
+
+import artwork2_1 from "../../assets/slidingPuzzle/3x3/artwork2/1.jpg";
+import artwork2_2 from "../../assets/slidingPuzzle/3x3/artwork2/2.jpg";
+import artwork2_3 from "../../assets/slidingPuzzle/3x3/artwork2/3.jpg";
+import artwork2_4 from "../../assets/slidingPuzzle/3x3/artwork2/4.jpg";
+import artwork2_5 from "../../assets/slidingPuzzle/3x3/artwork2/5.jpg";
+import artwork2_6 from "../../assets/slidingPuzzle/3x3/artwork2/6.jpg";
+import artwork2_7 from "../../assets/slidingPuzzle/3x3/artwork2/7.jpg";
+import artwork2_8 from "../../assets/slidingPuzzle/3x3/artwork2/8.jpg";
+import artwork2_9 from "../../assets/slidingPuzzle/3x3/artwork2/9.jpg";
+
+import artwork3_1 from "../../assets/slidingPuzzle/3x3/artwork3/1.jpg";
+import artwork3_2 from "../../assets/slidingPuzzle/3x3/artwork3/2.jpg";
+import artwork3_3 from "../../assets/slidingPuzzle/3x3/artwork3/3.jpg";
+import artwork3_4 from "../../assets/slidingPuzzle/3x3/artwork3/4.jpg";
+import artwork3_5 from "../../assets/slidingPuzzle/3x3/artwork3/5.jpg";
+import artwork3_6 from "../../assets/slidingPuzzle/3x3/artwork3/6.jpg";
+import artwork3_7 from "../../assets/slidingPuzzle/3x3/artwork3/7.jpg";
+import artwork3_8 from "../../assets/slidingPuzzle/3x3/artwork3/8.jpg";
+import artwork3_9 from "../../assets/slidingPuzzle/3x3/artwork3/9.jpg";
+
+const PuzzleImages = {
+  // 명절 아트웍
+  artwork1: {
+    1: artwork1_1,
+    2: artwork1_2,
+    3: artwork1_3,
+    4: artwork1_4,
+    5: artwork1_5,
+    6: artwork1_6,
+    7: artwork1_7,
+    8: artwork1_8,
+    9: artwork1_9,
+  },
+  // 나들이 아트웍
+  artwork2: {
+    1: artwork2_1,
+    2: artwork2_2,
+    3: artwork2_3,
+    4: artwork2_4,
+    5: artwork2_5,
+    6: artwork2_6,
+    7: artwork2_7,
+    8: artwork2_8,
+    9: artwork2_9,
+  },
+  // 라라 아트웍
+  artwork3: {
+    1: artwork3_1,
+    2: artwork3_2,
+    3: artwork3_3,
+    4: artwork3_4,
+    5: artwork3_5,
+    6: artwork3_6,
+    7: artwork3_7,
+    8: artwork3_8,
+    9: artwork3_9,
+  },
+};
+
+export default PuzzleImages;
