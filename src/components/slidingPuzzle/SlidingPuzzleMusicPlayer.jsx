c260159bf4c74f48690fb840c8234d3f61aa7fb9
LeeYongSoo <sideoff0217@naver.com>
2025-03-10 20:20:12 +0900

    feat: 슬라이딩 퍼즐 사운드 아이콘, 뮤트기능 추가

--- src/components/slidingPuzzle/SlidingPuzzleMusicPlayer.jsx
+++ src/components/slidingPuzzle/SlidingPuzzleMusicPlayer.jsx
@@ -2,34 +2,17 @@ import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import HennessysBGM from "../../assets/slidingPuzzle/backgroundMusic/Hennessys.mp3";
 import LithHarborBGM from "../../assets/slidingPuzzle/backgroundMusic/LithHarbor.mp3";
+import VolumeIcon from "../../assets/slidingPuzzle/Speaker.svg";
+import MuteIcon from "../../assets/slidingPuzzle/Mute.svg";
+import colors from "../common/color/colors";
 
 export const SlidingPuzzleMusicPlayer = () => {
   const [bgm, setBgm] = useState(HennessysBGM);
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(0.5);
+  const [prevVolume, setPrevVolume] = useState(0.5);
+  const [isMuted, setIsMuted] = useState(false);
   const audioRef = useRef(null);
-
-  useEffect(() => {
-    const audioElement = audioRef.current;
-    if (audioElement) {
-      const handleCanPlayThrough = () => {
-        if (isPlaying) {
-          audioElement.play().catch((error) => {
-            console.error("Failed to play audio:", error);
-          });
-        }
-      };
-      audioElement.addEventListener("canplaythrough", handleCanPlayThrough);
-      audioElement.load();
-
-      return () => {
-        audioElement.removeEventListener(
-          "canplaythrough",
-          handleCanPlayThrough
-        );
-      };
-    }
-  }, [bgm, isPlaying]);
 
   useEffect(() => {
     if (audioRef.current) {
@@ -53,25 +36,42 @@ export const SlidingPuzzleMusicPlayer = () => {
   };
 
   const handleVolumeChange = (event) => {
-    setVolume(event.target.value);
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    setPrevVolume(newVolume);
+    setIsMuted(newVolume === 0);
+  };
+
+  const toggleMute = () => {
+    if (isMuted) {
+      setVolume(prevVolume);
+    } else {
+      setPrevVolume(volume);
+      setVolume(0);
+    }
+    setIsMuted(!isMuted);
   };
 
   return (
     <>
       <label>
-        배경 음악 선택
-        <select value={bgm} onChange={handleBgmChange}>
+        배경 음악
+        <CustomSelect value={bgm} onChange={handleBgmChange}>
           <option value={HennessysBGM}>헤네시스</option>
           <option value={LithHarborBGM}>리스항구</option>
-        </select>
+        </CustomSelect>
+        <button onClick={togglePlayPause}>{isPlaying ? "❚❚" : "▶"}</button>
       </label>
       <CustomAudioPlayer>
         <audio ref={audioRef} loop>
           <source src={bgm} type="audio/mpeg" />
         </audio>
-        <button onClick={togglePlayPause}>{isPlaying ? "❚❚" : "▶"}</button>
         <label>
-          볼륨:
+          <VolumeIconStyled
+            src={isMuted ? MuteIcon : VolumeIcon}
+            alt="volume icon"
+            onClick={toggleMute}
+          />
           <input
             type="range"
             min="0"
@@ -88,8 +88,9 @@ export const SlidingPuzzleMusicPlayer = () => {
 
 const CustomAudioPlayer = styled.div`
   margin-top: 10px;
+
   button {
-    background-color: #ffaa33;
+    background-color: #ffffff;
     border: none;
     padding: 10px 10px;
     font-size: 16px;
@@ -101,13 +102,38 @@ const CustomAudioPlayer = styled.div`
       background-color: #ff8800;
     }
   }
+
   label {
-    display: block;
+    display: flex;
     margin-top: 10px;
     font-size: 16px;
   }
   input[type="range"] {
     width: 100%;
     margin-top: 5px;
+    cursor: pointer;
+  }
+`;
+
+const VolumeIconStyled = styled.img`
+  width: 40px;
+  height: 40px;
+  vertical-align: middle;
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
+const CustomSelect = styled.select`
+  margin-left: 10px;
+  padding: 5px 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  background-color: ${colors.main.white2};
+  border: 1px solid ${colors.commonInfo.normalBtn.btnBorder};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${colors.characterInfo.characterStatColor
+      .combatPowerBorder};
   }
 `;
