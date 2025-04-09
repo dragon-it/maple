e74e2c13b8a14f5990313587eab2d310c5fc3d9e
LeeYongSoo <sideoff0217@naver.com>
2025-04-09 21:29:27 +0900

    feat: 블레스 이미지 추가 (유니온 챔피언)

--- src/components/user/union/unionChampion/unionChampionimages.js
+++ src/components/user/union/unionChampion/unionChampionimages.js
@@ -19,6 +19,38 @@ import champion_insignia_3_1 from "../../../../assets/pages/user/union/champion/
 import champion_insignia_4_0 from "../../../../assets/pages/user/union/champion/insignia/champion_insignia_4_0.png";
 import champion_insignia_4_1 from "../../../../assets/pages/user/union/champion/insignia/champion_insignia_4_1.png";
 
+import chmapion_bless_archer_1 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.archer.layera1.png";
+import chmapion_bless_archer_2 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.archer.layera2.png";
+import chmapion_bless_archer_3 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.archer.layera3.png";
+import chmapion_bless_archer_4 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.archer.layera4.png";
+import chmapion_bless_archer_5 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.archer.layera5.png";
+
+import chmapion_bless_fighter_1 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.fighter.layera1.png";
+import chmapion_bless_fighter_2 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.fighter.layera2.png";
+import chmapion_bless_fighter_3 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.fighter.layera3.png";
+import chmapion_bless_fighter_4 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.fighter.layera4.png";
+import chmapion_bless_fighter_5 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.fighter.layera5.png";
+
+import chmapion_bless_pirate_1 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.pirate.layera1.png";
+import chmapion_bless_pirate_2 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.pirate.layera2.png";
+import chmapion_bless_pirate_3 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.pirate.layera3.png";
+import chmapion_bless_pirate_4 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.pirate.layera4.png";
+import chmapion_bless_pirate_5 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.pirate.layera5.png";
+
+import chmapion_bless_theif_1 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.theif.layera1.png";
+import chmapion_bless_theif_2 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.theif.layera2.png";
+import chmapion_bless_theif_3 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.theif.layera3.png";
+import chmapion_bless_theif_4 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.theif.layera4.png";
+import chmapion_bless_theif_5 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.theif.layera5.png";
+
+import chmapion_bless_wizard_1 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.wizard.layera1.png";
+import chmapion_bless_wizard_2 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.wizard.layera2.png";
+import chmapion_bless_wizard_3 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.wizard.layera3.png";
+import chmapion_bless_wizard_4 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.wizard.layera4.png";
+import chmapion_bless_wizard_5 from "../../../../assets/pages/user/union/champion/bless/championBlessing.statue.wizard.layera5.png";
+
+championBlessing.statue.theif.layera1;
+
 const card_Backgrnd = {
   empty: champion_card_Backgrnd_empty,
   disabled: champion_card_Backgrnd_disabled,
@@ -41,6 +73,44 @@ const insignia = {
   fifth: { 0: champion_insignia_4_0, 1: champion_insignia_4_1 },
 };
 
-const unionChampionImages = { card_Backgrnd, rank, insignia };
+const blessing = {
+  archer: {
+    1: chmapion_bless_archer_1,
+    2: chmapion_bless_archer_2,
+    3: chmapion_bless_archer_3,
+    4: chmapion_bless_archer_4,
+    5: chmapion_bless_archer_5,
+  },
+  fighter: {
+    1: chmapion_bless_fighter_1,
+    2: chmapion_bless_fighter_2,
+    3: chmapion_bless_fighter_3,
+    4: chmapion_bless_fighter_4,
+    5: chmapion_bless_fighter_5,
+  },
+  pirate: {
+    1: chmapion_bless_pirate_1,
+    2: chmapion_bless_pirate_2,
+    3: chmapion_bless_pirate_3,
+    4: chmapion_bless_pirate_4,
+    5: chmapion_bless_pirate_5,
+  },
+  theif: {
+    1: chmapion_bless_theif_1,
+    2: chmapion_bless_theif_2,
+    3: chmapion_bless_theif_3,
+    4: chmapion_bless_theif_4,
+    5: chmapion_bless_theif_5,
+  },
+  wizard: {
+    1: chmapion_bless_wizard_1,
+    2: chmapion_bless_wizard_2,
+    3: chmapion_bless_wizard_3,
+    4: chmapion_bless_wizard_4,
+    5: chmapion_bless_wizard_5,
+  },
+};
+
+const unionChampionImages = { card_Backgrnd, rank, insignia, blessing };
 
 export default unionChampionImages;
