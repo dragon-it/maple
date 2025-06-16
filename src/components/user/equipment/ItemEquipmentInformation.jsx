d06df71255f294ef74a1c320ea5a1791d78200be
LeeYongSoo <sideoff0217@naver.com>
2025-06-16 21:35:00 +0900

    fix: 기존 심볼 컴포넌트 삭제

--- src/components/user/equipment/ItemEquipmentInformation.jsx
+++ src/components/user/equipment/ItemEquipmentInformation.jsx
@@ -7,7 +7,6 @@ import androidEquipUi from "../../../assets/pages/user/equipment/equipmentUi/and
 import { ItemDetail } from "./ItemDetail";
 import gradeColors from "./ItemGradeColors";
 import { ItemSetEffect } from "./ItemSetEffect";
-import { ItemSymbol } from "./ItemSymbol";
 import { CashItemDetail } from "./CashItemDetail";
 import { PetItemDetail } from "./PetItemDetail";
 import { AndroidItemDetail } from "./AndroidItemDetail";
@@ -724,7 +723,6 @@ export const ItemEquipmentInformation = ({ EquipData }) => {
         </DetailWrap>
       </ItemInfoDetailWrap>
       <ItemSetEffect setinfo={EquipData.getSetEffect} />
-      <ItemSymbol symbolData={EquipData.getSymbol} />
       <SymbolUi symbolData={EquipData.getSymbol} />
     </Container>
   );
