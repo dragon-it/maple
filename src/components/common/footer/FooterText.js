0d1aa26c27c0b6b747ac7963dfed0bbc71ee22e2
LeeYongSoo <sideoff0217@naver.com>
2025-12-16 13:24:42 +0900

    refactor: FooterText 컴포넌트의 export 방식 수정

--- src/components/common/footer/FooterText.js
+++ src/components/common/footer/FooterText.js
@@ -1,22 +1,22 @@
 import React from "react";
 import OpenAPILogo from "../../../assets/logos/footerLogo.svg";
 
-const FooterText = () => (
-  <div className="footer-text">
-    <p>
-      Data By{" "}
-      <a href="https://openapi.nexon.com/ko/">
-        <img
-          src={OpenAPILogo}
-          alt="Data by OpenAPI"
-          style={{ width: "150px" }}
-        />
-      </a>
-    </p>
+export const FooterText = () => {
+  return (
+    <div className="footer-text">
+      <p>
+        Data By{" "}
+        <a href="https://openapi.nexon.com/ko/">
+          <img
+            src={OpenAPILogo}
+            alt="Data by OpenAPI"
+            style={{ width: "150px" }}
+          />
+        </a>
+      </p>
 
-    <p>Font By MapleStory</p>
-    <p>Contact: sideoff0217@naver.com</p>
-  </div>
-);
-
-export default FooterText;
+      <p>Font By MapleStory</p>
+      <p>Contact: sideoff0217@naver.com</p>
+    </div>
+  );
+};
