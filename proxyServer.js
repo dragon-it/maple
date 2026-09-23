1f2bdfd54bedc74a5db2fa1534d68c96852872e5
LeeYongSoo <sideoff0217@naver.com>
2026-09-23 17:25:36 +0900

    fix: 프론트엔드 API 키 노출 제거
    - Layout의 API 키 요청 헤더 제거
    - 미사용 apiEndPoint 유틸 삭제
    - 프록시의 키 환경변수를 NEXON_API_KEY로 변경

--- proxyServer.js
+++ proxyServer.js
@@ -30,7 +30,7 @@ const callMapleStoryAPI = async (endpoint, params) => {
         ...params,
       },
       headers: {
-        "x-nxopen-api-key": process.env.REACT_APP_API_KEY,
+        "x-nxopen-api-key": process.env.NEXON_API_KEY,
       },
     });
     if (response.status === 200) {
